5117f71b4c9f65d59044301b10e56a7212d37271
BBzzzr <allen-zhang-@outlook.com>
2021-01-03 03:38:59 -0600

    camera follow

--- Assets/Script/RubyController.cs
+++ Assets/Script/RubyController.cs
@@ -16,8 +16,8 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f*horizontal;
-        position.y = position.y + 0.1f*vertical;
+        position.x = position.x + 0.03f*horizontal;
+        position.y = position.y + 0.03f*vertical;
         transform.position = position;
     }
 }
